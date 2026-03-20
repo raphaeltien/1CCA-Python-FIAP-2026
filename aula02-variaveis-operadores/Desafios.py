5ef2d4273887aae19430bfa3161cb8f03a081203
raphaeltien <raphatien@gmail.com>
2026-03-20 11:31:47 -0300

    aula 2 variáveis e operadores

---
+++ aula02-variaveis-operadores/Desafios.py
@@ -0,0 +1,4 @@
+nome = input("Qual é o seu nome ?")
+idade = input("Qual é a sua idade ?")
+print(f"Boas vindas {nome} com {idade} anos !!")
+
---
+++ aula02-variaveis-operadores/aula02a.py
@@ -0,0 +1,33 @@
+print ("ola mundo")
+
+print (7 + 4)
+print ("7 + 4")
+print ("7" + "4" + "oi") #C CONCATENAÇÃO DE STRINGS
+
+#Comentário de 1 linha
+'''
+
+Comentários de
+múltiplas
+linhas
+'''
+# VARIÁVEIS
+nome = "Raphael"
+print(nome) # str
+idade = 17 # int
+peso = 76 # float
+
+print(nome, idade, peso)
+print (f"Eai {nome}!!!")
+print ("oiiiiii", nome, "!!!")
+
+# INPUT -SIMULAÇÃO DE UM FORMS NO CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(idade +1)
+
+
